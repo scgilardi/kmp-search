0b3e7ed6a2a07915d372bf84de02785fcd9da05e
Stephen C. Gilardi <scgilardi@gmail.com>
2014-09-18 15:30:43 -0400

    update to current clojure for dev

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha2"]]
   :global-vars {*warn-on-reflection* true}
   :java-source-paths ["java/src"]
   :jvm-opts [])
