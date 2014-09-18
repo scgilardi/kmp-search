ece645ddc1ec7c9eee527489ff3fc022fd8f1c1b
Stephen C. Gilardi <scgilardi@gmail.com>
2014-09-18 16:20:55 -0400

    simplify, removing clojure impl

--- src/kmp_search/core.clj
+++ src/kmp_search/core.clj
@@ -15,51 +15,10 @@
   (:require [clojure.java.io :as io])
   (:import (kmp_search Kernel)))
 
-(def byte-array-class (Class/forName "[B"))
-
 (defprotocol Search
   (search
     [this data]
     [this data limit]))
-
-(defmacro match-length
-  [data i pattern j border]
-  `(let [b# (aget ~data ~i)]
-     (-> (loop [~j ~j]
-           (if (or (neg? ~j) (= (aget ~pattern ~j) b#))
-             ~j
-             (recur (aget ~border ~j))))
-         (int)
-         (inc))))
-
-(defn border
-  [^bytes pattern]
-  {:pre [(isa? byte-array-class (class pattern))]}
-  (let [length (alength pattern)
-        border (int-array (inc length))]
-    (loop [i 0 j -1]
-      (aset-int border i j)
-      (when (< i length)
-        (recur (inc i) (match-length pattern i pattern j border))))
-    border))
-
-(defrecord KMP
-    [^bytes pattern ^int length ^ints border ^long offset ^int i ^int j]
-  Search
-  (search [this data]
-    (search this data (count data)))
-  (search [this data limit]
-    {:pre [(isa? byte-array-class (class data))
-           (<= limit (count data))]}
-    (let [data (bytes data)
-          limit (int limit)
-          [i j] (loop [i i j j]
-                  (if (and (< i limit) (< j length))
-                    (recur (inc i) (match-length data i pattern j border))
-                    [i j]))]
-      (if (= j length)
-        [(+ offset (- i j)) (assoc this :i i :j (aget border j))]
-        [nil (assoc this :offset (+ offset i) :i 0 :j j)]))))
 
 (extend-type Kernel
   Search
@@ -67,14 +26,9 @@
     ([this data]
        (search this data (count data)))
     ([this data limit]
-       (.search this data limit))))
-
-(defn _context [^bytes pattern]
-  {:pre [(isa? byte-array-class (class pattern))]}
-  (->KMP pattern (count pattern) (border pattern) 0 0 0))
+       (vec (.search this data limit)))))
 
 (defn context [^bytes pattern]
-  {:pre [(isa? byte-array-class (class pattern))]}
   (Kernel. pattern))
 
 (defn search-file
